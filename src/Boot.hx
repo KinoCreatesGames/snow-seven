48078e4bef1042c49e93d6901317eac4d84c8ab9
kinor <kinoarose@gmail.com>
2021-12-16 12:31:44 -0500

    clean up rendering and disable rendering unnecessary elements when the game is on the title screen

--- src/Boot.hx
+++ src/Boot.hx
@@ -93,40 +93,39 @@ class Boot extends hxd.App {
   @:access(h3d.scene.Scene, h3d.scene.Renderer, CustomRenderer)
   override function render(e:Engine) {
     // Grab Render Texture for the 2D scene so that can make modifications
-    var renderTarget = new Texture(engine.width, engine.height, [Target]);
-    // Composite Shader
-    var compShader = new CompositeShader(new TextureArray(engine.width,
-      engine.height, 3, [Target]));
-    engine.pushTarget(compShader.textures, 2);
-    engine.clear(0, 1);
-
-    // Disable level snow before render texture screen shader pass
+    // Level Rendering
     if (Game.ME != null && Game.ME.level != null) {
       var level = Game.ME.level;
+      var shader = mode7;
+      var renderTarget = new Texture(engine.width, engine.height, [Target]);
+      // Composite Shader
+      var compShader = new CompositeShader(new TextureArray(engine.width,
+        engine.height, 3, [Target]));
+      engine.pushTarget(compShader.textures, 2);
+      engine.clear(0, 1);
+
+      // Disable level snow before render texture screen shader pass
       Game.ME.scroller.visible = true;
       level.snow.visible = false;
-    }
-    s2d.render(e);
-    engine.popTarget();
-    // Dsiable level before rendering snow
-    if (Game.ME != null && Game.ME.level != null) {
-      var level = Game.ME.level;
+      s2d.render(e);
+      engine.popTarget();
+
+      // Dsiable level before rendering snow
       level.snow.visible = true;
       Game.ME.scroller.visible = false;
+
+      engine.pushTarget(compShader.textures, 1);
+      engine.clear(0, 1);
+      s2d.render(e);
+      engine.popTarget();
+
+      // Compsite for all textures passed into the texture array
+      ScreenFx.run(shader, compShader.textures, 0);
+      ScreenFx.run(compShader, freeze.texture, 0);
+      // Color Screen
+      new ScreenFx(freeze).render();
+    } else {
+      super.render(e);
     }
-    engine.pushTarget(compShader.textures, 1);
-    engine.clear(0, 1);
-    s2d.render(e);
-    engine.popTarget();
-    var shader = mode7;
-    // shader.worldPos.y += 0.0009;
-    // shader.worldPos.x -= 0.01;
-    ScreenFx.run(shader, compShader.textures, 0);
-
-    ScreenFx.run(compShader, freeze.texture, 0);
-    // Compsite for all textures passed into the texture array
-    // Color Screen
-
-    new ScreenFx(freeze).render();
   }
 }
